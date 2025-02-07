a67a56f9fff1c6ca80d241446a491cd158367f9c
Zahid Gazi <152292012+ZahidGazi@users.noreply.github.com>
2025-02-07 19:02:08 +0530

    added testcase for config

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,7 +1,5 @@
 import os
-import subprocess
-import sys
-import webbrowser
+from logger import logger
 
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QPushButton, QMessageBox
 from PyQt6.QtGui import QIcon
@@ -17,13 +15,20 @@ class MainWindow(QDialog):
         self.setLayout(self.layout)
         self.resize(300, 200)
 
+        # Button to launch the lyrics widget
         self.add_widget_button = QPushButton("Add Lyrics Widget")
         self.add_widget_button.clicked.connect(self.launch_widget)
         self.layout.addWidget(self.add_widget_button)
 
+        # Button to clear the config file
         self.clear_config_button = QPushButton("Clear Config")
         self.clear_config_button.clicked.connect(self.clear_config)
         self.layout.addWidget(self.clear_config_button)
+        
+        # Button to change configuration
+        self.change_config_button = QPushButton("Change Config")
+        self.change_config_button.clicked.connect(self.change_config)
+        self.layout.addWidget(self.change_config_button)
 
         config = load_config()
         if not config:
@@ -31,13 +36,18 @@ class MainWindow(QDialog):
             self.sp_oauth = None
             self.token_info = None
         else:
-            self.sp_oauth = create_spotify_oauth(
-                config["SPOTIPY_CLIENT_ID"],
-                config["SPOTIPY_CLIENT_SECRET"],
-                config["SPOTIPY_REDIRECT_URI"]
-            )
-            self.token_info = self.sp_oauth.get_cached_token()
-
+            try:
+                self.sp_oauth = create_spotify_oauth(
+                    config["SPOTIPY_CLIENT_ID"],
+                    config["SPOTIPY_CLIENT_SECRET"],
+                    config["SPOTIPY_REDIRECT_URI"]
+                )
+                self.token_info = self.sp_oauth.get_cached_token()
+            except Exception as e:
+                logger.error(f"Failed to authenticate: {e}")
+                QMessageBox.critical(self, "Error", "Failed to authenticate: Please run the setup again.")
+                self.sp_oauth = None
+                self.token_info = None
     def launch_widget(self):
         if not self.token_info:
             QMessageBox.critical(self, "Error", "No token found. Please reauthenticate.")
@@ -56,3 +66,9 @@ class MainWindow(QDialog):
                 QMessageBox.critical(self, "Error", f"Failed to clear config: {e}")
         else:
             QMessageBox.information(self, "Clear Config", "No configuration file exists.")
+
+    def change_config(self):
+        from ui.setup_window import SetupWindow
+        self.hide()  # Hide current MainWindow
+        self.setup_window = SetupWindow()
+        self.setup_window.show()
